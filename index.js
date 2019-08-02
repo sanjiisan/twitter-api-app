8c132c9cc66cb24bea8c23fb4260531ed5426dd4
Paweł Wójcik <pawel.wojcik@schibsted.com>
2019-08-02 19:36:52 +0200

    travis

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-    res.write('DZIAŁAM!!!'); //write a response to the client
+    res.write('KO<MUNIKAT'); //write a response to the client
     res.end(); //end the response
 }).listen(process.env.PORT || 8080); //the server object listens on port 8080
